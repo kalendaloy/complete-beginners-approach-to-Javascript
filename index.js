aa695972a42301e64497c8238cc8641a19bf96e0
kalendaloy <141020957+kalendaloy@users.noreply.github.com>
2023-09-28 23:42:47 +0300

    application setup
    application setup - initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app=express();
+
+const port = 20007;
+
+//app.listenportNumber,function)
+app.listen(port, ()=> {
+                        console.log('listening to port 20007')
+                         })
